b915f53aee4cd51d518068da186df5935b55dd7b
Diego Alfredo Ruiz Cabezas <diego.ruiz3@virginiogomez.cl>
2025-04-12 19:46:13 -0400

    feater: No carga la tabla T.T

--- app.py
+++ app.py
@@ -6,30 +6,47 @@ app = Flask(__name__)
 app.secret_key = 'd94231c751543589c4619656fb1781f28bc9452632e5977ef76660c352bc5da7'
 
 recordatorioList = []              ## lista
-idActual = str(uuid.uuid4())       ## id UUID
+idActual = int(uuid.uuid4())       ## id UUID
 CreateAt= int(time.time() * 1000)  ## Creado el "dia-mes-año"
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/reminders")
-def Cargar_reminders():
-    return render_template("Recordatorios.html")
+@app.route("/Recordatorio")
+def quienes_somos():
+    return render_template("Recordatorios.html", recordatorio = recordatorioList)
 
-@app.post("/crear_reminders")
-def crear_reminders():
+@app.post("/crear-recordatorio")
+def crear_tarea():
     global idActual
     datos = request.form
     texto = datos.get("texto")
 
     if texto is None or len(texto) < 5:
-        flash("El texto del recordatorio debe tene como mínimo 5 caracteres", "warning")
-        return redirect("Recordatorios.html")
+        flash("El texto de la tarea debe tene como mínimo 5 caracteres", "warning")
+        return redirect("/Recordatorio")
     
-    nueva_reminders = nueva_reminders = { "id": idActual, "texto": texto,"CreatAt":CreateAt, "completado": False }
-    recordatorioList.append(nueva_reminders)
+    nueva_tarea = nueva_tarea = { "id": idActual, "texto": texto, "CreateAt": CreateAt,"Importante": False }
+    idActual += 1
+    recordatorioList.append(nueva_tarea)
 
     flash("Recordatorio creada exitosamente", "success")
-    return nueva_reminders, 201
+    return redirect("/Recordatorio")
 
+
+@app.route("/api/Recordatorio/<int:id>", methods=["PATCH"])
+def api_actualizar_tarea(id):
+    for tarea in recordatorioList:
+        if tarea["id"] == id:
+            tarea["Importante"] = not tarea["Importante"]
+            return tarea
+    return "Recordatorio no encontrada", 404
+
+@app.route("/api/Recordatorio/<int:id>", methods=["DELETE"])
+def api_borrar_Recordatorio(id):
+    for index, Recordatorio in enumerate(recordatorioList):
+        if Recordatorio["id"] == id:
+            del recordatorioList[index]
+            return '', 204
+    return "Recordatorio no encontrada", 404
