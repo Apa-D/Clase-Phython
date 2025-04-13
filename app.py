bee2a063ac193920fef260421a225ef2ae00054f
Diego Alfredo Ruiz Cabezas <diego.ruiz3@virginiogomez.cl>
2025-04-13 11:18:44 -0400

    feater: T.T sigue sin cargar la tabla

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app.secret_key = 'd94231c751543589c4619656fb1781f28bc9452632e5977ef76660c352bc5d
 
 recordatorioList = []              ## lista
 idActual = int(uuid.uuid4())       ## id UUID
-CreateAt= int(time.time() * 1000)  ## Creado el "dia-mes-año"
+CreateAt= int(time.time() * 1000)  ## Creado el 
 
 @app.route("/")
 def index():
@@ -18,35 +18,36 @@ def quienes_somos():
     return render_template("Recordatorios.html", recordatorio = recordatorioList)
 
 @app.post("/crear-recordatorio")
-def crear_tarea():
+def crear_recordatorio():
     global idActual
     datos = request.form
     texto = datos.get("texto")
 
     if texto is None or len(texto) < 5:
-        flash("El texto de la tarea debe tene como mínimo 5 caracteres", "warning")
+        flash("El texto del recordatorio debe tene como mínimo 5 caracteres", "warning")
         return redirect("/Recordatorio")
     
-    nueva_tarea = nueva_tarea = { "id": idActual, "texto": texto, "CreateAt": CreateAt,"Importante": False }
+    nueva_redimers = nueva_redimers = { "id": idActual, "texto": texto, "CreateAt": CreateAt,"Importante": False }
     idActual += 1
-    recordatorioList.append(nueva_tarea)
+    recordatorioList.append(nueva_redimers)            #no da error
 
-    flash("Recordatorio creada exitosamente", "success")
+    flash("Recordatorio creada exitosamente", "success") 
     return redirect("/Recordatorio")
 
 
-@app.route("/api/Recordatorio/<int:id>", methods=["PATCH"])
-def api_actualizar_tarea(id):
-    for tarea in recordatorioList:
-        if tarea["id"] == id:
-            tarea["Importante"] = not tarea["Importante"]
-            return tarea
+@app.route("/Recordatorio/<int:id>", methods=["PATCH"])
+def actualizar_recordatorio(id):
+    for redimers in recordatorioList:
+        if redimers["id"] == id:
+            redimers["Importante"] = not redimers["Importante"]
+            return redimers
     return "Recordatorio no encontrada", 404
 
-@app.route("/api/Recordatorio/<int:id>", methods=["DELETE"])
-def api_borrar_Recordatorio(id):
-    for index, Recordatorio in enumerate(recordatorioList):
-        if Recordatorio["id"] == id:
+
+@app.route("/Recordatorio/<int:id>", methods=["DELETE"])
+def borrar_Recordatorio(id):
+    for index, redimers in enumerate(recordatorioList):
+        if redimers["id"] == id:
             del recordatorioList[index]
             return '', 204
     return "Recordatorio no encontrada", 404
